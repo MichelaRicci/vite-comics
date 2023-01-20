dab219a30a319e4ccd104d53a3a0f92993fca72f
MichelaRicci <michela.ricci.1991@gmail.com>
2023-01-20 17:30:56 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,49 @@
-<script setup>
+<script>
 
 import AppHeader from './components/AppHeader.vue'
 import AppMain from './components/AppMain.vue'
 import AppFooter from './components/AppFooter.vue'
 
+export default {
+  data() {
+    return {
+
+    }
+  },
+
+  components: { AppHeader, AppMain, AppFooter }
+}
+
 </script>
 
 <template>
+  <header>
+    <AppHeader></AppHeader>
+  </header>
 
-  <AppHeader></AppHeader>
-  <AppMain></AppMain>
-  <AppFooter></AppFooter>
+  <main>
+    <AppMain></AppMain>
+  </main>
+
+  <footer>
+    <AppFooter></AppFooter>
+  </footer>
 
 </template>
 
-<style scoped>
+<style>
+/* Reset */
 
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+/* Generics */
+
+.container {
+  width: 1200px;
+  margin: 0 auto;
+}
 </style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,9 +1,120 @@
 <script>
+
+export default {
+    name: 'AppHeader',
+
+    data() {
+        return {
+            links: [
+                {
+                    text: 'Characters',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Comics',
+                    url: '#',
+                    current: true,
+                },
+                {
+                    text: 'Movies',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'TV',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Games',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Collectibles',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Videos',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Fans',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'News',
+                    url: '#',
+                    current: false,
+                },
+                {
+                    text: 'Shop',
+                    url: '#',
+                    current: false,
+                },
+            ]
+        }
+    }
+}
 </script>
 
 <template>
+
+    <header>
+        <nav class="container">
+            <div class="nav-left">
+                <img src="../assets/img/dc-logo.png" alt="dc-logo">
+            </div>
+            <div class="nav-right">
+                <ul>
+                    <li v-for="link in links" :key="link.text">
+                        <a :class="{ 'active': link.current }" :href="link.url">{{ link.text }}</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
 </template>
 
-<style>
+<style lang="scss" scoped>
+nav {
+    height: 100px;
+    display: flex;
+}
 
+.nav-left {
+    width: 40%;
+
+    display: flex;
+    align-items: center;
+}
+
+.nav-right {
+    width: 60%;
+
+    ul {
+        height: 100%;
+        display: flex;
+        align-items: center;
+
+
+        li {
+            list-style-type: none;
+
+            a {
+                display: inline-block;
+
+                padding: 5px;
+
+
+            }
+        }
+
+    }
+}
 </style>
